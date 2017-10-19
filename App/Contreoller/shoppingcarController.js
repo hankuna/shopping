d3842516394d366ada5fdd82071e8b177f07dfb9
unknown <>
2017-10-19 10:26:37 +0800

    hh

---
+++ App/Contreoller/shoppingcarController.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/10/18.
+ */
+app.controller("shoppingcarController",function($scope){
+    $scope.tabs=[];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ App/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/10/18.
+ */
+
+var app=angular.module("app",["ui.router"]);
+
+app.config(function($stateProvider,$urlRouterProvider){
+    $stateProvider
+        .state("shoppingcar",{
+            url:"/shoppingcar",
+            templateUrl:"./App/View/shoppingcar.html",
+            controller:"shoppingcarController"
+        });
+    $urlRouterProvider.otherwise("shoppingcar");
+});
